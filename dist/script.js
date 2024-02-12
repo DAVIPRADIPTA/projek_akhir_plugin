9cd29ed09fb2da84b9a87cabbbe8c667c4a12a05
DAVIPRADIPTA <davipradipta456@gmail.com>
2024-02-12 10:34:19 +0700

    projek akhir

--- dist/script.js
+++ dist/script.js
@@ -21,9 +21,7 @@ fetch('https://pokeapi.co/api/v2/pokemon/?limit=1000')
 
     for(let i = 1 ; i <= 40 ; i++){
         const dataPokemon = daftarPokemon[i];
-    // }
-
-    // daftarPokemon.forEach(dataPokemon => {
+  
         fetch(dataPokemon.url)
         .then(res => {
             if(!res.ok){
@@ -33,11 +31,7 @@ fetch('https://pokeapi.co/api/v2/pokemon/?limit=1000')
         })
         .then(pokemon =>{
             const getData = document.importNode(template.content, true);
-            // const pokemonTypes = pokemon.types.map(type => type.type.name);
-            // const pokemonAbilities = pokemon.abilities.map(ability => ability.ability.name);
-
-         
-            // const p =document.querySelector('p')
+           
             
             getData.querySelector('img').src = pokemon.sprites.front_shiny;
             getData.querySelector('img').alt = pokemon.name;
@@ -66,40 +60,3 @@ fetch('https://pokeapi.co/api/v2/pokemon/?limit=1000')
 
 
 
-// const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=40';
-
-// fetch(apiUrl)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Terjadi kesalahan saat mengambil data dari API Pokemon');
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     // Ambil daftar Pokemon
-//     const pokemonList = data.results;
-
-//     // Ambil tipe dari setiap Pokemon
-//     pokemonList.forEach(pokemon => {
-//       fetch(pokemon.url)
-//         .then(response => {
-//           if (!response.ok) {
-//             throw new Error('Terjadi kesalahan saat mengambil data dari API Pokemon');
-//           }
-//           return response.json();
-//         })
-//         .then(pokemonData => {
-          
-//           const pokemonTypes = pokemonData.types.map(type => type.type.name);
-//           console.log(`Tipe ${pokemonData.name}:`, pokemonTypes);
-//           const getData = document.importNode(template.content, true)
-//           getData.querySelector('h2').textContent = pokemonData.pokemonTypes;
-//         })
-//         .catch(error => {
-//           console.error('Ada kesalahan saat mengambil data Pokemon:', error);
-//         });
-//     });
-//   })
-//   .catch(error => {
-//     console.error('Ada kesalahan:', error);
-//   });
